a9912a7db87d40c3a5cf93019d8db31d17e6b5c5
pesricha <naman_p@me.iitr.ac.in>
2025-08-18 03:01:36 +0530

    static in classes

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "hello.h"
 
+void Haters::f() {}; // this will export
+int Haters::c = 0; // this will export
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    a();
     return 0;
 }
--- cpp/hello.h
+++ cpp/hello.h
@@ -2,4 +2,6 @@ class Haters {
 public:
     int a;
     void b();
+    static int c; // class variable
+    static void f(); // static method but export 'this' is not passed
 };
