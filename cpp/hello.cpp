cd2f3c9a0fd0e7ab8fb88b118d1c4b4e70895c0d
pesricha <naman_p@me.iitr.ac.in>
2025-09-05 03:15:35 +0530

    memory layout

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
+#include <ostream>
 #include "hello.h"
 
-template <class T>
-void b(T) {
-    
-}
+class B {
+public:
+    B() {};
+    int b;
+    // virtual void c() = 0;
+};
+
+class C {
+public:
+    int c;
+};
+
+class  A: public B, public C{
+public:
+    A() {};
+    int a;
+    // virtual void c();
+};
+
+// Memory layout
+// A
+// B       C
+// int b   int c   int a
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    b<int>(5); 
-    b<char>('5');
-    return 0;
+    A a;
+    B *b = &a;
+    C *c = &a;
+
+    std::cout << - (unsigned long long)(&a) + (unsigned long long)b << - (unsigned long long)(&a) + (unsigned long long)c  << std::endl;
+
+    std::cout << sizeof(A) << std::endl;
 }
